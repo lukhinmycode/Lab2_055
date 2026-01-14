de56a5fa1aa19da0953674ed3da57f2e9aeeb5cb
lukhinmycode <kittipop.m@rmutsvmail.com>
2026-01-14 10:59:40 +0700

    edit 3

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
 			</td>
 			<td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรกddddddd</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
+				<li><a href="room.php">ห้องพัก&ราคาาาาาาา</li>
 				<li><a href="facility.php">สิ่งอำนวยความสะดวก</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อvvvv</li>
